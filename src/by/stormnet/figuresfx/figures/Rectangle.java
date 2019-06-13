274ee9363530115be39c7d7d1d923abfb06242b1
Anastasia-Kruk <anastasiakruk1985@gmail.com>
2019-06-13 16:52:37 +0300

    Paint figures (Java FX)

---
+++ src/by/stormnet/figuresfx/figures/Rectangle.java
@@ -0,0 +1,72 @@
+package by.stormnet.figuresfx.figures;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.Objects;
+
+public class Rectangle extends Figure {
+    private double width;
+    private double higth;
+
+    public Rectangle(double cx, double cy, double lineWidth, Color color) {
+        super(FIGURE_TYPE_RECTANGLE, cx, cy, lineWidth, color);
+    }
+
+    public Rectangle(double cx, double cy, double lineWidth, Color color, double width, double higth) {
+        this(cx, cy, lineWidth, color);
+        this.width = width;
+        this.higth = higth;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHigth() {
+        return higth;
+    }
+
+    public void setHigth(double higth) {
+        this.higth = higth;
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+        gc.setLineWidth(lineWidth);
+        gc.setStroke(color);
+        gc.strokeRect(cx - width / 2, cy - higth / 2, cx + width / 2, cy + higth / 2);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(rectangle.width, width) == 0 &&
+                Double.compare(rectangle.higth, higth) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, higth);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Rectangle{");
+        sb.append("width=").append(width);
+        sb.append(", higth=").append(higth);
+        sb.append(", cx=").append(cx);
+        sb.append(", cy=").append(cy);
+        sb.append(", lineWidth=").append(lineWidth);
+        sb.append(", color=").append(color);
+        sb.append('}');
+        return sb.toString();
+    }
+}
+
---
+++ src/by/stormnet/figuresfx/figures/Triangle.java
@@ -0,0 +1,64 @@
+package by.stormnet.figuresfx.figures;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.Objects;
+
+public class Triangle extends Figure {
+    private double lenght;
+
+    public Triangle(double cx, double cy, double lineWidth, Color color) {
+        super(FIGURE_TYPE_TRIANGLE, cx, cy, lineWidth, color);
+    }
+
+    public Triangle(double cx, double cy, double lineWidth, Color color, double lenght) {
+        this(cx, cy, lineWidth, color);
+        this.lenght = lenght;
+    }
+
+    public double getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(double lenght) {
+        this.lenght = lenght;
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+        gc.setLineWidth(lineWidth);
+        gc.setStroke(color);
+        gc.beginPath();
+        gc.moveTo(cx + lenght / 2, cy + lenght / 2);
+        gc.lineTo(cx - lenght / 2, cy + lenght / 2);
+        gc.lineTo(cx, cy - lenght / 2);
+        gc.closePath();
+        gc.stroke();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return Double.compare(triangle.lenght, lenght) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lenght);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Triangle{");
+        sb.append("lenght=").append(lenght);
+        sb.append(", cx=").append(cx);
+        sb.append(", cy=").append(cy);
+        sb.append(", lineWidth=").append(lineWidth);
+        sb.append(", color=").append(color);
+        sb.append('}');
+        return sb.toString();
+    }
+}
